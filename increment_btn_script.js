b5050bb749efb5307359395c1aebb3fbb35aa96f
Roscasey <roscasey@hotmail.com>
2024-06-01 13:44:26 +1000

    update on my increment btn : now the decrease part also works bc i moved the update price function outside

--- increment_btn_script.js
+++ increment_btn_script.js
@@ -3,6 +3,16 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const qty = document.getElementById('qty_num');
     const dec_btn = document.getElementById('btn_minus');
     const add_btn = document.getElementById('btn_plus');
+    
+    let item_price_element = document.querySelector('.cart_product_price'); //get the cart product price
+    let item_price = 9.95; //the price of all cards 
+    
+    let update_price = () => {
+        let current_qty = parseInt(qty.value); 
+        let add_it =  (item_price * current_qty).toFixed(2); //round decimal points 
+        item_price_element.innerHTML = '$' + add_it; //add the $ symbol back
+        console.log(add_it); // tester output
+    } 
 
     dec_btn.addEventListener('click', ()=> {
         if (qty.value <= 1) {
@@ -11,8 +21,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
         else {
             qty.value = parseInt(qty.value) - 1; //turn into a number and decrease by 1 per click 
         }
+        update_price();
     });
     add_btn.addEventListener('click', ()=> {
         qty.value = parseInt(qty.value) + 1; //turn into a number and increase by 1 per click
+        update_price();
     });
 });
+  
