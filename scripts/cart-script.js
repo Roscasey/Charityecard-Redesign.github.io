4a3e349420e931a23850b6761cce6c8a187d80e0
Roscasey <roscasey@hotmail.com>
2024-06-03 21:23:57 +1000

    updated the functions, got rid of duplicate id within forms and made generalised classes. Changed payment confirmation page title and progression bar current page.

--- scripts/cart-script.js
+++ scripts/cart-script.js
@@ -2,21 +2,23 @@
 
 // getting all the document elements for each card 
 const classicItem = document.getElementById('classic_item');
+const classicItemHr = document.getElementById('classic_item_hr');
 const classicChristmasCharity = sessionStorage.getItem("classicChristmasCharity");
 const seasonsGreetingCharity = sessionStorage.getItem("seasonsCharity")
 
 const seasonsGreetingCharityElement = document.getElementById('seasons_greeting_charity');
-const classicChristmasCharityElement = document.getElementById('classic_christmans_charity');
+const classicChristmasCharityElement = document.getElementById('classic_christmas_charity');
 
 //getting the total_box sub / total 
-const subTotalPriceElement = document.getElementById("subtotal_price");
-const totalPriceElement = document.getElementById("total_price");
+const subTotalPriceElement = document.getElementById('subtotal_price');
+const totalPriceElement = document.getElementById('total_price');
 
 seasonsGreetingCharityElement.innerHTML = seasonsGreetingCharity;
 
 // display classic christmas only if it is added to cart
 if (classicChristmasCharity === null) {
     classicItem.style.display = "none";
+    classicItemHr.style.display = "none"; //the line below classic christmas product
 }
 else {
     classicItem.style.display = "";
